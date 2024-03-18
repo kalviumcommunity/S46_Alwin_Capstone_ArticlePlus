ec63badbd36dade072b196bd3e25ad7c1da35569
Alwin Sunil <64636626+AlwinSunil@users.noreply.github.com>
2024-03-18 09:29:40 +0530

    Fixed bug in user db schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: {
         type: String,
-        function() {
+        required: function () {
             return this.provider === "email"
         },
     },
